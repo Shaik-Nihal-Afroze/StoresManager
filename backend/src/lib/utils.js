4200c763284e950a38fcf97fb3bffa1fa832fabb
Shaik Nihal Afroze <nihal8722@gmail.com>
2025-06-23 15:24:06 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,8 +6,8 @@ export const generateToken = (userId,res)=>{
         res.cookie('jwtToken',token,{
             maxAge:7*24*60*60*1000, //in milliseconds
             httpOnly:true,
-            sameSite:'Lax',
-            secure:process.env.NODE_ENV !=="development"
+            sameSite:'None',
+            secure:true
         })
         
         return token
